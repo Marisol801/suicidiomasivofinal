d522545de7125449b9f5cb99c73b25637f51c447
Saul <saul_suarez12@hotmail.com>
2018-05-16 22:12:44 -0500

    nani

--- serverxd.js
+++
@@ -1,7 +0,0 @@
-let http = require('http');
-
-http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify({ Estatus: 'deleted', error_code: -1 }));
-    res.end();
-}).listen(80);
