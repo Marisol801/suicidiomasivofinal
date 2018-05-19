0122b0f483fcff00351eada5ea9185df957a6e7b
Saul <saul_suarez12@hotmail.com>
2018-05-19 13:05:14 -0500

    se agrego la base de los usuarios xdxd

--- app.js
+++ app.js
@@ -16,9 +16,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //require('./server/routes')(app);
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('*', (req, res) => res.status(200).send({
-    message: 'Welcome to the beginning of nothingness.',
-}));
 
 require('./server/routes')(app);
 
